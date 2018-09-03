5f74ccb65a54a9835b375d9a26c494837ed5e269
VictorVasquezP <42286571+VictorVasquezP@users.noreply.github.com>
2018-09-02 20:59:21 -0500

    Add files via upload

---
+++ Aleking.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main(int argc, char *argv[]) {
+	int *vec,n,i,o,j,min,aux;
+	scanf("%d",&n);
+	vec=(int*)malloc(n*sizeof(int));
+	for(i=0;i<n;i++){
+		scanf("%d",&vec[i]);
+	}
+	scanf("%d",&o);
+	switch(o){
+	case 1:
+		for(i=n-1;i>=0;i--){
+			printf("%d ",vec[i]);
+		}
+		break;
+	case 2:
+		for(i=0;i<n;i++){
+			printf("%d ",vec[i]);
+		}
+		break;
+	case 3:
+		for(i=0;i<n;i++){
+			min=i;
+			for(j=i+1;j<n;j++){
+				if(vec[j]<vec[min]){
+					min=j;
+				}
+			}
+			aux=vec[i];
+			vec[i]=vec[min];
+			vec[min]=aux;
+		}
+		for(i=n-1;i>=0;i--){
+			printf("%d ",vec[i]);
+		}
+		break;
+	default:
+		break;
+	}
+	free(vec);
+	return 0;
+}
+
